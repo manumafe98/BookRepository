b9d6056ccfca7e5e5839e9f3c492025f55b72687
manumafe98 <manumaxerapro@gmail.com>
2023-12-01 19:51:39 -0300

    Using lombook annotations for apiresponse model

--- backend/book_repo/src/main/java/com/manumafe/book_repo/model/ApiResponse.java
+++ backend/book_repo/src/main/java/com/manumafe/book_repo/model/ApiResponse.java
@@ -1,17 +1,13 @@
 package com.manumafe.book_repo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApiResponse {
+
     private String message;
-
-    public ApiResponse(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
