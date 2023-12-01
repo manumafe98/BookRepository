9b6edb775c4a640a7b5836910b86710431ee7bcd
manumafe98 <manumaxerapro@gmail.com>
2023-12-01 15:06:33 -0300

    Adding error handling in the backend for the authentication and registration of users

---
+++ backend/book_repo/src/main/java/com/manumafe/book_repo/exceptions/LoginException.java
@@ -0,0 +1,7 @@
+package com.manumafe.book_repo.exceptions;
+
+public class LoginException extends RuntimeException {
+    public LoginException(String message) {
+        super(message);
+    }
+}
---
+++ backend/book_repo/src/main/java/com/manumafe/book_repo/exceptions/RegistrationException.java
@@ -0,0 +1,7 @@
+package com.manumafe.book_repo.exceptions;
+
+public class RegistrationException extends RuntimeException {
+    public RegistrationException(String message) {
+        super(message);
+    }
+}
